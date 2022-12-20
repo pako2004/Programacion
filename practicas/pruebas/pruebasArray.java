e4635be1c0bb41f8b44cdd44f69dcbb8bf4b81e2
pako2004 <lrodmat346y@g.educaand.es>
2022-12-20 14:38:14 +0100

    marditasea

--- practicas/pruebas/pruebasArray.java
+++ practicas/pruebas/pruebasArray.java
@@ -1,20 +1,40 @@
 package pruebas;
 
+
+
 import arrays.UtilesArrayLuis;
 
 public class pruebasArray {
     public static void main(String[] args) {
-        
 
         int[] array = new int[10];
         array[1] = 10;
+        array[9]=10;
 
-        array = UtilesArrayLuis.limipiar();
+        
 
-        if (array == null){
-            System.out.println("siu");
+        //array = UtilesArrayLuis.limipiar();.
+
+
+        int acum = 0;
+        array = UtilesArrayLuis.insertaAlInicio(array, 77);
+
+        for (int i = 0; i < array.length; i++){
+
+            System.out.println(i+"- "+array[i]);
+            acum++;
         }
+        System.out.println(acum);
+        /* 
+        int[] array = null;
 
+        UtilesArrayLuis.rellenar(10);
+
+        UtilesArrayLuis.imprimir(array);
+
+        array = UtilesArrayLuis.limpiar();*/
+
+        //se esta creando mas posiciones de array pero estan vacias 
 
     }
 }
